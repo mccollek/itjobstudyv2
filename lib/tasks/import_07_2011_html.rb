e3efc5ce5d28ef533a9d7b1e575a38bc90d16e78
Karl McCollester <karlmccollester@udhaa.com>
2013-12-10 13:39:09 -0500

    interim check-in

--- lib/tasks/import_07_2011_html.rb
+++ lib/tasks/import_07_2011_html.rb
@@ -1,14 +1,17 @@
-require 'nokogiri'
-require 'open-uri'
+  require 'nokogiri'
+  require 'open-uri'
 
-#get doc
-doc = Nokogiri::HTML(open('http://www.bls.gov/oes/2011/may/oes_nat.htm'))
+  #get doc
+  doc = Nokogiri::HTML(open('http://www.bls.gov/oes/2011/may/oes_nat.htm'))
 
 #get all rows of first table as xml
 doc.xpath('//table[1]/tbody/tr')
 
 #get first row of first table as xml
-doc.xpath('//table[1]/tbody/tr[1]')
+header_hash =  this.get_headers(doc.xpath('//table[1]/tbody/tr[1]'))
+
+
+pp header_hash
 
 #get the string value of the first cell of the first row
 doc.xpath('//table[1]/tbody/tr[1]/td[1]/text()').to_s
@@ -17,3 +20,11 @@ doc.xpath('//table[1]/tbody/tr[1]/td[1]/text()').to_s
 #national and state for each year is a separate URL.  Load the URLs from text file.  better, crawl http://www.bls.gov/oes/tables.htm for national state links
 #TO-DO - lots of values have notes; what to do with each
 #TO-DO - 2011 table differs from 2010 and before.  How can I read the table headers to figure out what column goes where?
+
+def this.get_headers (header_row)
+  heading_hash = Hash.new
+  header_row.first.children.each_with_index do |heading, index|
+    value = heading.children.first.text.strip!.presence
+    heading_hash[index] = value
+  end
+end
