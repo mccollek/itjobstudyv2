5319de7730e98d95091d891045fdb5086f07dfbe
Karl McCollester <karlmccollester@udhaa.com>
2013-12-26 15:19:03 -0500

    interim check-in: mining all valid URLs from 2011-2013.  Need to modify BlsOesSpider.has_data? to find valid pages in 2009.

--- app/services/bls_data_importer.rb
+++ app/services/bls_data_importer.rb
@@ -1,9 +1,10 @@
 class BlsDataImporter
   require 'nokogiri'
   require 'open-uri'
-  attr_reader :source
+  attr_reader :source, :year
   def initialize attributes
     @source = attributes.fetch(:source)
+    @year = attributes.fetch(:year)
     unless self.open_source
       raise ArgumentError, 'Sorry, the URL was not found'
       return false
@@ -15,16 +16,12 @@ class BlsDataImporter
     doc = self.open_source
     #get all rows of first table as xml
     doc.xpath('//table[1]/tbody/tr')
-
     #get first row of first table as xml
     header_hash =  self.get_headers(doc.xpath('//table[1]/thead/tr[1]'))
-
-
     pp header_hash
-
     #get the string value of the first cell of the first row
-    doc.xpath('//table[1]/tbody/tr[1]/td[1]/text()').to_s
-
+    first_val = doc.xpath('//table[1]/tbody/tr[1]/td[1]/text()').to_s
+    print "\n \n first value of page #{self.source} for year #{year} is #{first_val} \n\n"
   end
 
   def open_source
--- app/services/bls_oes_spider.rb
+++ app/services/bls_oes_spider.rb
@@ -1,11 +1,12 @@
 class BlsOesSpider
   require 'nokogiri'
   require 'open-uri'
-  attr_reader :source_page
+  attr_reader :source_page, :candidate_pages
   def initialize args = {}
     @source_page = args.fetch(:source) do
       @source_page = 'http://www.bls.gov/oes/tables.htm'
     end
+    @candidate_pages = []
   end
 
   def open_source
@@ -15,13 +16,52 @@ class BlsOesSpider
   end
 
   def build_candidate_page_list
+    self.reset_candidate_pages
     doc = self.open_source
     results = doc.xpath('//a[contains(text(), "HTML")]')
-    candidate_pages = []
     results.each do |result|
-      pp "result is #{result} \n"
-      candidate_pages.push(result.attributes.first[1].value)
+      child_obj = BlsOesSpider.new(source: 'http://www.bls.gov' + result.attributes.first[1].value)
+      print "looking at #{child_obj.source_page} \n"
+      if child_obj.has_data?
+        self.add_candidate(child_obj.source_page)
+      else
+        child_doc = child_obj.open_source
+        state_list = child_doc.xpath('//blockquote/li/a')
+        unless state_list.nil?
+          child_path = Pathname.new(child_obj.source_page)
+          state_dir = child_path.dirname.to_s
+          state_list.each do |state_source|
+            state_filename = state_source.first[1]
+            state_page = BlsOesSpider.new(source: state_dir + '/' + state_filename)
+            print "looking at #{state_page.source_page} \n"
+            if state_page.has_data?
+              self.add_candidate(state_page)
+            end
+          end
+        end
+      end
+      print "we now have #{self.candidate_pages.count} candidate pages \n"
     end
-    return candidate_pages
+  end
+
+  def reset_candidate_pages
+    @candidate_pages = []
+  end
+
+  def add_candidate(source)
+    self.candidate_pages.push(source)
+  end
+
+  def has_data?
+    #TODO: works for 2011-2013... have to fix with 2010
+    doc = self.open_source
+
+    if doc.xpath('//table[1]/tbody/tr[1]/td[1]/text()').present?
+      print "#{self.source_page} has data\n"
+      return true
+    else
+      print "#{self.source_page} does not have data\n"
+      return false
+    end
   end
 end
