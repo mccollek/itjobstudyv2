895b0326d4993650f4830e3db77d67941c6f7ec8
Karl McCollester <karlmccollester@udhaa.com>
2013-12-27 14:08:13 -0500

    Pulling all valid URLs 2004-2013

--- app/services/bls_oes_spider.rb
+++ app/services/bls_oes_spider.rb
@@ -20,22 +20,30 @@ class BlsOesSpider
     doc = self.open_source
     results = doc.xpath('//a[contains(text(), "HTML")]')
     results.each do |result|
-      child_obj = BlsOesSpider.new(source: 'http://www.bls.gov' + result.attributes.first[1].value)
-      print "looking at #{child_obj.source_page} \n"
-      if child_obj.has_data?
-        self.add_candidate(child_obj.source_page)
-      else
-        child_doc = child_obj.open_source
-        state_list = child_doc.xpath('//blockquote/li/a')
-        unless state_list.nil?
-          child_path = Pathname.new(child_obj.source_page)
-          state_dir = child_path.dirname.to_s
-          state_list.each do |state_source|
-            state_filename = state_source.first[1]
-            state_page = BlsOesSpider.new(source: state_dir + '/' + state_filename)
-            print "looking at #{state_page.source_page} \n"
-            if state_page.has_data?
-              self.add_candidate(state_page)
+      high_level_url =  result.attributes.first[1].value
+      if high_level_url.count('may')>0    #I don't want the november numbers or anything before 2003
+        child_obj = BlsOesSpider.new(source: 'http://www.bls.gov' + high_level_ur)
+        print "looking at #{child_obj.source_page} \n"
+        if child_obj.has_data?
+          self.add_candidate(child_obj.source_page)
+        else
+          child_doc = child_obj.open_source
+          state_list = child_doc.xpath('//blockquote/li/a')
+          unless state_list.nil?
+            child_path = Pathname.new(child_obj.source_page)
+            state_dir = child_path.dirname.to_s
+            state_list.each do |state_source|
+              state_filename = state_source.first[1]
+              if state_filename.count('/')>0
+                state_url = 'http://www.bls.gov' + state_filename
+              else
+                state_url = state_dir + '/' + state_filename
+              end
+              state_page = BlsOesSpider.new(source: state_url)
+              print "looking at lower level #{state_page.source_page} \n"
+              if state_page.has_data?
+                self.add_candidate(state_page)
+              end
             end
           end
         end
@@ -56,7 +64,7 @@ class BlsOesSpider
     #TODO: works for 2011-2013... have to fix with 2010
     doc = self.open_source
 
-    if doc.xpath('//table[1]/tbody/tr[1]/td[1]/text()').present?
+    if doc.at_xpath('//td[preceding-sibling::td[text()="00-0000"]]/text()').present?
       print "#{self.source_page} has data\n"
       return true
     else
