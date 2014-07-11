2281343ab25c30a33c869d48c676f099846123b9
Karl McCollester <karlmccollester@udhaa.com>
2014-07-11 09:38:28 -0400

    set strong params on report data

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -69,6 +69,6 @@ class ReportsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def report_params
-      params.require(:report).permit(:title)
+      params.require(:report).permit(:title, area_ids: [], occupation_ids: [])
     end
 end
